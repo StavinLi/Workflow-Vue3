1e9d6b537992bafd12b5593956fa58f7a1fbb34c
lichen39 <lichen39@58.com>
2022-10-25 19:45:28 +0800

    feat: 打包更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       component:()=>import('@/views/setting.vue')
-      }
+    }
   ]
 })
 
--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,48 @@
-import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, "src"),
-      '~@': path.resolve(__dirname, "src"),
-    }
-  },
-  css: {
-    preprocessorOptions: {
-        less: {
-            modifyVars: {},
-            javascriptEnabled: true
-        },
-        scss: {
-            additionalData: `@import "src/styles/global.scss";`
-        }
-    }
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  build: {
-
-    /** If you set esmExternals to true, this plugins assumes that 
-      all external dependencies are ES modules */
- 
-    commonjsOptions: {
-       esmExternals: true 
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode, __dirname)
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, "src"),
+        '~@': path.resolve(__dirname, "src"),
+      }
     },
- }
+    css: {
+      preprocessorOptions: {
+          less: {
+              modifyVars: {},
+              javascriptEnabled: true
+          },
+          scss: {
+              additionalData: `@import "src/styles/global.scss";`
+          }
+      }
+    },
+    base: env.VITE_MODE === "production" ? '/Workflow-Vue3/dist/' : '/',
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    build: {
+  
+      /** If you set esmExternals to true, this plugins assumes that 
+        all external dependencies are ES modules */
+   
+      commonjsOptions: {
+         esmExternals: true 
+      },
+   }
+  }
 })
